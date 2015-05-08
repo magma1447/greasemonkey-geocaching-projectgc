06ab8d70d9a6c53d7d1d6419f5f6dd04641c4980
Bas Bosman <github@nazgul.nu>
2015-05-08 13:26:47 +0200

    Move checker(s) to sidebar

--- greasemonkey-geocaching-projectgc.user.js
+++ greasemonkey-geocaching-projectgc.user.js
@@ -245,12 +245,12 @@
 	                    if(challengeCheckerTagIds.length > 0) {
 	                    	var html = '';
 
-	                    	html += '<div id="PGC_ChallengeCheckers">';
+	                    	html += '<div id="checkerWidget" class="CacheDetailNavigationWidget TopSpacing BottomSpacing"><h3 class="WidgetHeader">Checker(s)</h3><div class="WidgetBody" id="PGC_ChallengeCheckers">';
 	                    	for(var i = 0 ; i < challengeCheckerTagIds.length ; i++) {
-	                    		html += '<a href="http://project-gc.com/Challenges//' + challengeCheckerTagIds[i] + '"><img src="http://maxcdn.project-gc.com/Images/Checker/' + challengeCheckerTagIds[i] + '" title="Project-GC Challenge checker" alt="PGC Checker"></a>';
+	                    		html += '<a href="http://project-gc.com/Challenges//' + challengeCheckerTagIds[i] + '" style="display: block; width: 200px; margin: 0 auto;"><img src="http://maxcdn.project-gc.com/Images/Checker/' + challengeCheckerTagIds[i] + '" title="Project-GC Challenge checker" alt="PGC Checker"></a>';
 	                    	}
-	                    	html += '</div>';
-		                    $('#ctl00_ContentBody_CacheInformationTable').append(html)
+	                    	html += '</div></div>';
+		                    $('#map_preview_canvas').before(html);
 		                }
 
                     }
