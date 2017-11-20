605d4ecc3f2ca33e1f5642b7ac22e65a2059b41c
Magnus Månsson <magma@1447.se>
2017-11-20 10:26:30 +0100

    v2.0.0 release
    Rewritten to work with  Greasemonkey 4/Firefox.

--- greasemonkey-geocaching-projectgc.user.js
+++ greasemonkey-geocaching-projectgc.user.js
@@ -14,7 +14,7 @@
 // @include     http://www.geocaching.com/*
 // @include     https://www.geocaching.com/*
 // @exclude     https://www.geocaching.com/profile/profilecontent.html
-// @version     1.8.6
+// @version     2.0.0
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
 // @require     https://greasyfork.org/scripts/5392-waitforkeyelements/code/WaitForKeyElements.js?version=19641
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
