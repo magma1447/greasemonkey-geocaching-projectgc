7204c0840f7ab227bd4543edfa66eafa1343c715
Magnus Månsson <magma@1447.se>
2022-05-29 21:29:15 +0200

    hotfix

--- greasemonkey-geocaching-projectgc.user.js
+++ greasemonkey-geocaching-projectgc.user.js
@@ -17,7 +17,7 @@
 // @include     http://www.geocaching.com/*
 // @include     https://www.geocaching.com/*
 // @exclude     https://www.geocaching.com/profile/profilecontent.html
-// @version     2.3.13
+// @version     2.3.14
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
 // @require	https://greasyfork.org/scripts/383527-wait-for-key-elements/code/Wait_for_key_elements.js?version=701631
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
@@ -950,8 +950,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
                         listId;
 
                     html += '<select id="comboVGPS" style="width: 138px;">';
-                    for (k in vgpsLists) {
-                        var listId  vgpsLists[k].id;
+                    for (var k in vgpsLists) {
+                        listId = vgpsLists[k].id;
 
                         selectedContent = '';
                         if (+selected === +listId) {
