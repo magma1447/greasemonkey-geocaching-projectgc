935a783eb9027add6a5cd9c5a04c7536cc61da0b
Magnus Månsson <magma@1447.se>
2023-10-28 14:58:15 +0200

    Proper (hopefully) locale fix

--- greasemonkey-geocaching-projectgc.user.js
+++ greasemonkey-geocaching-projectgc.user.js
@@ -19,7 +19,7 @@
 // @match           https://www.geocaching.com/*
 // @exclude         https://www.geocaching.com/profile/profilecontent.html
 // @exclude         https://www.geocaching.com/help/*
-// @version         2.4.8
+// @version         2.4.9
 // @require         http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
 // @require         https://greasyfork.org/scripts/383527-wait-for-key-elements/code/Wait_for_key_elements.js?version=701631
 // @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
@@ -100,7 +100,7 @@
         .init({
             supportedLngs: ['ca_ES', 'cs_CZ', 'da_DK', 'de_DE', 'en_AU', 'en_CA', 'en_GB', 'en_US', 'es_ES', 'fi_FI', 'fr_FR', 'hu_HU', 'it_IT', 'ko_KR', 'lv_LV', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'sk_SK', 'sl_SI', 'sv_SE', 'tr_TR', 'zh_TW'],
             whitelist: ['ca_ES', 'cs_CZ', 'da_DK', 'de_DE', 'en_AU', 'en_CA', 'en_GB', 'en_US', 'es_ES', 'fi_FI', 'fr_FR', 'hu_HU', 'it_IT', 'ko_KR', 'lv_LV', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'sk_SK', 'sl_SI', 'sv_SE', 'tr_TR', 'zh_TW'],
-            preload: ['ca_ES', 'cs_CZ', 'da_DK', 'de_DE', 'en_AU', 'en_CA', 'en_GB', 'en_US', 'es_ES', 'fi_FI', 'fr_FR', 'hu_HU', 'it_IT', 'ko_KR', 'lv_LV', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'sk_SK', 'sl_SI', 'sv_SE', 'tr_TR', 'zh_TW'],
+            //preload: ['ca_ES', 'cs_CZ', 'da_DK', 'de_DE', 'en_AU', 'en_CA', 'en_GB', 'en_US', 'es_ES', 'fi_FI', 'fr_FR', 'hu_HU', 'it_IT', 'ko_KR', 'lv_LV', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'sk_SK', 'sl_SI', 'sv_SE', 'tr_TR', 'zh_TW'],
             fallbackLng: ['en_US', 'nb_NO', 'sv_SE'],
             'lng': navigator.language || navigator.userLanguage,
             ns: ['userscript'],
@@ -111,11 +111,9 @@
             }
         }, (err, t) => {
             if (err) {
-                if (err.indexOf("failed parsing" > -1)) {
-                    if(lng != 'en_US') {
-                        i18next.changeLanguage('en_US');
-                        return loadTranslations();
-                    }
+                if (err.indexOf('failed parsing') > -1 && err.indexOf('en_US') > -1) {
+                    i18next.changeLanguage('en_US');
+                    return loadTranslations();
                 }
                 return console.log("Error occurred when loading language data", err);
             }
